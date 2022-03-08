99d9986b0c6fe196c38a998644f43a90cda0ab1c
akshayaravindh1015 <akshaynetha1015@gmail.com>
2022-03-08 19:30:56 +0530

    Theming entire App

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ThemeService } from '@layout/services/theme.service';
+
 @Component({
   selector: 'cust-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private _themeServc: ThemeService) { }
 
   ngOnInit(): void {
+  }
+
+  toggleTheme(event: Event) {
+    event.preventDefault();
+    if (this._themeServc.isDarkTheme()) {
+      this._themeServc.setLightTheme();
+    } else {
+      this._themeServc.setDarkTheme();
+    }
   }
 
 }
