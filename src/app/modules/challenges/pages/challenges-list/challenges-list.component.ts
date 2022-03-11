433bb8d8f99f72211e13cd13056b28fc267d5993
akshayaravindh1015 <akshaynetha1015@gmail.com>
2022-03-11 17:24:26 +0530

    Sorting Challenges

--- src/app/modules/challenges/pages/challenges-list/challenges-list.component.ts
+++ src/app/modules/challenges/pages/challenges-list/challenges-list.component.ts
@@ -6,22 +6,47 @@ import { AppState, challenegsList$ } from '@store/index';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-challenges-list',
+  selector: 'challenges-list',
   templateUrl: './challenges-list.component.html',
   styleUrls: ['./challenges-list.component.scss'],
 })
 export class ChallengesListComponent implements OnInit, OnDestroy {
   challenges: Challenge[] = [];
   subscription!: Subscription;
+  sortFun = (c1: Challenge, c2: Challenge) => c2.upVotesCount - c1.upVotesCount;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.subscription = this.store
       .select(challenegsList$)
-      .subscribe((list) => (this.challenges = list));
+      .subscribe((list) => this.setSortedChallenges(list));
   }
 
+  setSortedChallenges(challenges: Challenge[]) {
+    this.challenges = challenges.slice().sort(this.sortFun);
+  }
+  changeSortFun(event: Event) {
+    switch ((event.target as HTMLSelectElement).value) {
+      case 'dateAsc':
+        this.sortFun = (c1: Challenge, c2: Challenge) =>
+          c1.createdAt > c2.createdAt ? 1 : -1;
+        break;
+      case 'dateDesc':
+        this.sortFun = (c1: Challenge, c2: Challenge) =>
+          c1.createdAt > c2.createdAt ? -1 : 1;
+        break;
+      case 'votesAsc':
+        this.sortFun = (c1: Challenge, c2: Challenge) =>
+          c1.upVotesCount - c2.upVotesCount;
+        break;
+      case 'votesDesc':
+        this.sortFun = (c1: Challenge, c2: Challenge) =>
+          c2.upVotesCount - c1.upVotesCount;
+        break;
+    }
+    this.setSortedChallenges(this.challenges);
+  }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
